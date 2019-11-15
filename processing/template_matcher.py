5b371e5044523a5cd173b55711b543001a4ce371
ahmad-aljabali <ahmad-aljabali@protonmail.com>
2019-11-16 02:54:59 +0300

    modified template matcher

--- processing/template_matcher.py
+++ processing/template_matcher.py
@@ -1,44 +1,41 @@
 import numpy as np
 import cv2
 
-
-def template_matcher(image,template_path):
+def template_matcher(image,template_path, confidence_thresh=0, steps=5):
     template = cv2.imread(template_path)
     template = cv2.resize(template, (0,0), fx = 0.2, fy=0.2)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     template = cv2.Canny(template, 50, 200)
+    cv2.imshow('template', template)
     (tH, tW) = template.shape[:2]
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     found = None
+    result = None
 
-    # loop over the scales of the image
-    for scale in np.linspace(0.2, 1.0, 10)[::-1]:
-        # resize the image according to the scale, and keep track
-        # of the ratio of the resizing
+    # loop over multiple scales of the image
+    for scale in np.linspace(0.2, 1.0, steps)[::-1]:
         resized = cv2.resize(gray, (0,0), fx = scale, fy=scale)
-        r = gray.shape[1] / float(resized.shape[1])
+        r = 1/scale
 
-        # if the resized image is smaller than the template, then break
-        # from the loop
+        # break if the resized image is smaller than the template
         if resized.shape[0] < tH or resized.shape[1] < tW:
             break
         edged = cv2.Canny(resized, 50, 200)
-        result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+        matches = cv2.matchTemplate(edged, template, method=cv2.TM_CCOEFF_NORMED)
+        (_, confidence, _, maxLoc) = cv2.minMaxLoc(matches)
 
 
-        # if we have found a new maximum correlation value, then update
-        # the bookkeeping variable
-        if found is None or maxVal > found[0]:
-            found = (maxVal, maxLoc, r)
+        # save the best fit match
+        if found is None or confidence > found[0]:
+            found = (confidence, maxLoc, r)
 
-    # unpack the bookkeeping variable and compute the (x, y) coordinates
-    # of the bounding box based on the resized ratio
-    (_, maxLoc, r) = found
-    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+    # calculate the bounding box based on the scale ratio
+    (confidence, maxLoc, r) = found
+    (resultX, resultY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+    (resultW, resultH) = (int(tW * r), int(tH * r))
 
 
-    bbox = [startX, startY, endX-startX, endY-startY]
-    return bbox
+    if confidence >= confidence_thresh:
+        result = [resultX, resultY, resultW, resultH, confidence]
+    return result
