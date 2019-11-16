5eb3ab5e33255ddd84efce4e40982f90df6f32c1
ahmad-aljabali <36141448+ahmad-aljabali@users.noreply.github.com>
2019-11-16 04:25:14 +0300

    Update color_mask.py

--- processing/color_mask.py
+++ processing/color_mask.py
@@ -5,18 +5,16 @@ import numpy as np
 def color_mask(frame,colors):
     #initialize & optain parameters
     kernel_size= (5,5)
-    first=True
     mask=None
 
     for color in colors:
         #in case of multiple ranges given for a color itirate for every range and compine all masks
         for r in color:
             #first color mask
-            if first == True:
+            if mask == None:
                 mask = cv2.inRange(frame, r[0], r[1])
                 mask = cv2.erode(mask,kernel_size)
                 mask = cv2.dilate(mask,kernel_size)
-                first=False
             #add the new mask to the total mask
             else:
                 mask_it = cv2.inRange(frame, r[0], r[1])
