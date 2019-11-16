862a4bb9206de480fd1136e71e6766e8a24d7dc2
ahmad-aljabali <ahmad-aljabali@protonmail.com>
2019-11-16 23:16:36 +0300

    update README.md

--- processing/template_matcher.py
+++ processing/template_matcher.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def template_matcher(image,template_path, confidence_thresh=0, steps=5):
+def template_matcher(frame,template_path, confidence_thresh=0, steps=5):
     template = cv2.imread(template_path)
     template = cv2.resize(template, (0,0), fx = 0.2, fy=0.2)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -9,16 +9,16 @@ def template_matcher(image,template_path, confidence_thresh=0, steps=5):
     cv2.imshow('template', template)
     (tH, tW) = template.shape[:2]
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     found = None
     result = None
 
-    # loop over multiple scales of the image
+    # loop over multiple scales of the frame
     for scale in np.linspace(0.2, 1.0, steps)[::-1]:
         resized = cv2.resize(gray, (0,0), fx = scale, fy=scale)
         r = 1/scale
 
-        # break if the resized image is smaller than the template
+        # break if the resized frame is smaller than the template
         if resized.shape[0] < tH or resized.shape[1] < tW:
             break
         edged = cv2.Canny(resized, 50, 200)
