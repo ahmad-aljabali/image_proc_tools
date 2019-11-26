105a254ea9914447d892fa074315998853904268
ahmad-aljabali <36141448+ahmad-aljabali@users.noreply.github.com>
2019-11-26 22:55:46 +0300

    Update get_position.py

--- stereo_processing/get_position.py
+++ stereo_processing/get_position.py
@@ -4,13 +4,13 @@ import numpy as np
 # this function takes world_pts, bounding box*[x,y,width,hight] and/or mask* (* = optional input)
 def get_position(world_pts, bbox=None, input_mask=None):
 
+    height, width, _ = world_pts.shape
     mask1 = np.zeros((height,width), np.uint8)
     mask2 = np.zeros((height,width), np.uint8)
     # use provided roi formats
     if input_mask != None:
         mask1 = input_mask
     if bbox != None:
-        height, width, _ = world_pts.shape
         mask2[int(bbox[1]):int(bbox[1]) + int(bbox[3]),int(bbox[0]):int(bbox[0]) + int(bbox[2])]=(1)
 
     # mask out missing values from world_pts
